66e3f2e0a8304a72a072a06ad0ac9add373bfd4a
USER <user@user-no-macbook-pro.local>
2017-02-22 00:27:12 +0900

    first commit

--- Swift_AdMob/ViewController.swift
+++ Swift_AdMob/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,GADBannerViewDelegate{
+    
+    
+    @IBOutlet weak var bannerView: GADBannerView!
+    let request = GADRequest()
+    
+    var inter:GADInterstitial!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        request.testDevices = [kGADSimulatorID]
+        bannerView.delegate = self
+        bannerView.adUnitID = "ca-app-pub-1973574128490282/7735447659"
+        bannerView.rootViewController = self
+        bannerView.load(request)
+        
+        inter = GADInterstitial(adUnitID: "ca-app-pub-1973574128490282/9212180853")
+        inter.load(request)
+        
     }
 
+    @IBAction func showInter(_ sender: Any) {
+        
+        inter.present(fromRootViewController: self)
+        request.testDevices = [kGADSimulatorID]
+        inter = GADInterstitial(adUnitID: "ca-app-pub-1973574128490282/9212180853")
+        inter.load(request)
+
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
